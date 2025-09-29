549b6ff733770f64b4dc7fc2f2b79297051abf46
Sanjiv Simha <sasimha@ucsc.edu>
2025-09-29 11:52:24 -0700

    loading wordpress content with javascript

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'b434851ef354f37ea4f1');
+<?php return array('dependencies' => array('jquery'), 'version' => '35c3abcbd9cfe2e25095');
