38bfc60b81b2ebc32736743a2878eb1d064afd00
Sanjiv Simha <sasimha@ucsc.edu>
2025-10-09 14:58:51 -0700

    custom layout for professors and events in search overlay

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '241b23d2fb92abbca663');
+<?php return array('dependencies' => array('jquery'), 'version' => '71178a3cc4171b430a63');
