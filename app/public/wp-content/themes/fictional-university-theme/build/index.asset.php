187adad597b0c7092eb73ab6b2f068dd512909cc
Sanjiv Simha <sasimha@ucsc.edu>
2026-04-28 11:42:11 -0700

    working on delete button

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'b063ec2db91d96c42741');
+<?php return array('dependencies' => array('jquery'), 'version' => '80e6a9b9beaa48a02202');
