9e6da5074372970f012b3861e10863b19a0addab
Sanjiv Simha <sasimha@ucsc.edu>
2026-05-29 11:16:55 -0700

    javascript plugin development, custom wp block dev

---
+++ app/public/wp-content/plugins/are-you-paying-attention/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+/*
+    Plugin Name: Are You Paying Attention Quiz
+    Description: A truly amazing plugin.
+    Version: 1.0
+    Author: Sanjiv
+    Author URI: https://sanjivsimha.com
+*/
+
+if (!defined('ABSPATH')) exit; // exit if accessed directly
+
+class AreYouPayingAttention {
+    function __construct() {
+        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    }
+
+    function adminAssets() {
+        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks', 'wp-element'));
+    }
+
+}
+
+$areYouPayingAttention = new AreYouPayingAttention();
