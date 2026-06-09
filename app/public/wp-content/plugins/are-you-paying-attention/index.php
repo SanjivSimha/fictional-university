c60d15e17225a70f9b1d01bab9e82f3b302a4c0b
Sanjiv Simha <sasimha@ucsc.edu>
2026-06-09 13:52:28 -0700

    block output

--- app/public/wp-content/plugins/are-you-paying-attention/index.php
+++ app/public/wp-content/plugins/are-you-paying-attention/index.php
@@ -12,13 +12,23 @@ if (!defined('ABSPATH')) exit; // exit if accessed directly
 
 class AreYouPayingAttention {
     function __construct() {
-        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+        add_action('init', array($this, 'adminAssets'));
     }
 
     function adminAssets() {
-        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+        register_block_type('ourplugin/are-you-paying-attention', array(
+            'editor_script' => 'ournewblocktype',
+            'render_callback' => array($this, 'theHTML')
+        ));
     }
 
+    function theHTML($attributes) {
+        ob_start(); ?>
+        <p>Today the sky is <span class="skyColor"><?php echo $attributes['skyColor']; ?></span> and the grass is <span class="grassColor"><?php echo $attributes['grassColor']; ?></span>.</p>
+        <?php
+        return ob_get_clean();
+    }
 }
 
 $areYouPayingAttention = new AreYouPayingAttention();
